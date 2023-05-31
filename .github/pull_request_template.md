b2398ab67068152b623395afbfcef2dc868c4459
Maciej Urbanski <maciej.urbanski+gh@reef.pl>
2023-05-31 21:27:01 +0200

    add `nox -s format`

---
+++ .github/pull_request_template.md
@@ -0,0 +1,4 @@
+<!--
+Please ensure run `nox -s format` locally before committing changes.
+You can remove this comment after from PR description.
+-->
---
+++ noxfile.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+
+import nox
+
+
+CI = os.environ.get('CI') is not None
+
+ROOT = Path('.')
+PYTHON_VERSIONS = ['3.9']
+PYTHON_DEFAULT_VERSION = PYTHON_VERSIONS[-1]
+
+nox.options.default_venv_backend = 'venv'
+nox.options.stop_on_first_error = True
+nox.options.reuse_existing_virtualenvs = True
+
+# In CI, use Python interpreter provided by GitHub Actions
+if CI:
+    nox.options.force_venv_backend = 'none'
+
+
+@nox.session(python=PYTHON_DEFAULT_VERSION)
+def format(session):
+    session.run(
+        'docker',
+        'run',
+        '--rm',
+        '-v', f'{ROOT.absolute()}:/data',
+        '-w', '/data',
+        '-u', f'{os.geteuid()}:{os.getegid()}',
+        'ghcr.io/bobheadxi/readable:v0.5.0@sha256:423c133e7e9ca0ac20b0ab298bd5dbfa3df09b515b34cbfbbe8944310cc8d9c9',
+        'fmt', '**/*.md',
+    )
